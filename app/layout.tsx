9c97620b41724dbcfffb10622a9abac797dde427
Mun Hong <munhong07@gmail.com>
2024-06-07 12:27:48 +0800

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+      <body className={montserrat.className}>
+          {children}
+      </body>
     </html>
   );
 }
